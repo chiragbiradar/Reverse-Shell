7d5fa6d37d0115d29ec30b077bec231d743a7535
chiragbiradar <chiragsb16@gmail.com>
2023-07-15 01:26:12 +0530

    Binding the Sockets

--- Single_Client/server.py
+++ Single_Client/server.py
@@ -14,3 +14,19 @@ def create_Socket():
     except socket.error as msg:
         print("Socket creation error: "+str(msg))
 
+# Binding the sockets and listening for connections
+def bind_socket():
+    try:
+        global host
+        global port
+        global sct
+
+        print("Socket creation error: "+str(port))
+
+        sct.bind((host,port))
+        sct.listen(5)
+
+    except socket.error as msg:
+        print("Socket Binding errror: " + str(msg) + "\n" + "Retrying...")
+        bind_socket()
+
