f15622e95f2e5db84ca2a8cb1bd0c3a739b55b8b
chiragbiradar <chiragsb16@gmail.com>
2023-07-15 22:33:19 +0530

    added client file

--- Single_Client/server.py
+++ Single_Client/server.py
@@ -2,50 +2,64 @@ import socket
 import sys
 
 # Creating a Socket
-def create_Socket():
+def create_socket():
     try:
         global host
-        global port 
-        global sct 
+        global port
+        global s
         host = ""
         port = 9999
-        sct = socket.socket()
+        s = socket.socket()
 
     except socket.error as msg:
-        print("Socket creation error: "+str(msg))
+        print("Socket creation error: "+ str(msg))
 
 # Binding the sockets and listening for connections
+
+
 def bind_socket():
     try:
         global host
         global port
-        global sct
+        global s
 
-        print("Socket creation error: "+str(port))
+        print("Binding the port: " + str(port))
 
-        sct.bind((host,port))
-        sct.listen(5)
+        s.bind((host, port))
+        s.listen(5)
 
     except socket.error as msg:
         print("Socket Binding errror: " + str(msg) + "\n" + "Retrying...")
         bind_socket()
 
-# Establishing connection with a client 
+# Establishing connection with a client
+
+
 def socket_accept():
-    conn,address = sct.accept()
-    print("Connection has been Established! |" + " IP " + address[0] + " | Port" + str(address[1]))
+    conn, address = s.accept()
+    print("Connection has been Established! |" + " IP " +
+          address[0] + " | Port" + str(address[1]))
     send_command(conn)
     conn.close()
+
 
 def send_command(conn):
     while True:
         cmd = input()
         if cmd == 'quit':
             conn.close()
-            sct.close()
+            s.close()
             sys.exit()
         if len(str.encode(cmd)) > 0:
             conn.send(str.encode(cmd))
-            client_response = str(conn.recv(1024),"utf-8")
-            print(client_response,end="")
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end="")
 
+
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+
+
+main()
